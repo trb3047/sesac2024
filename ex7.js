73d6ca5fa66469142b8c823e9325ea98523604a0
blklime <hoon_1215@naver.com>
2024-06-28 16:05:55 +0900

    proxy, stack queue extends 풀이

---
+++ ex7.js
@@ -0,0 +1,221 @@
+// Emp type의 hong 객체에 fullName 기능을 Accessor Property를 사용하지 말고, proxy 생성자 함수를 이용하여 구현하시오.
+const assert = require('assert');
+
+class Emp {
+    firstName;
+    lastName;
+}
+
+const hong = new Proxy(new Emp, {
+    get (target, prop) {
+        if(prop === 'fullName') {
+            return `${target.firstName} ${target.lastName.toUpperCase()}`;
+        } else if (prop === 'lastName') {
+            return target[prop]?.toUpperCase();
+        } else {
+            return target[prop];
+        }
+    },
+    set (target, prop, value) {
+        if(prop === 'fullName') {
+            const v = value.split(' ');
+            if (v.length >= 2) {
+                target.firstName = v[0];
+                target.lastName = v[1];
+            } else {
+                target.lastName = v[0];
+            }
+        } else {
+            return target[prop] = value;
+        }
+        return target;
+    }
+})
+
+hong.fullName = 'Kildong Hong'; // split하여 firstName, lastName 셋
+console.log(hong.fullName);     // 'Kildong HONG' 출력하면 통과!
+hong.fullName = 'Lee';
+console.log(hong.firstName, hong.lastName);  // 'Kildong LEE' 출력하면 통과!
+
+///////////////////////////////////////////////////////////////////
+
+
+// 모든 Array가 다음 기능을 갖도록 구현하세요.
+// 1) mapBy(), findBy(), filterBy(), rejectBy(), sortBy()
+// 2) firstObject, lastObject
+
+const hong2 = { id: 1, name: 'Hong' };
+const kim = { id: 2, name: 'Kim' };
+const lee = { id: 3, name: 'Lee' };
+const users = [hong2, lee, kim];
+const arr = [1, 2, 3, 4, 5]; // arr.firstObject; // 1    arr.lastObject; // 5
+
+Array.prototype.mapBy = function (k) {
+    return this.map((v) => v[k]);
+}
+Array.prototype.findBy = function (k, v) {
+    return this.find((obj) => obj[k] === v);
+}
+Array.prototype.filterBy = function (k, v) {
+    return this.filter((obj) => obj[k] === v);
+}
+Array.prototype.rejectBy = function (k, v) {
+    return this.filter((obj) => obj[k] !== v);
+}
+Array.prototype.sortBy = function (k) {
+    let sortType = 0;
+    let key = (k.match('desc')) ? (sortType = -1, k.split(':desc')[0]) : k;
+    let ret = this.map((val) => val[key]).sort();
+    if (sortType < 0) ret.reverse();
+    this.map((v) => {
+        for (let e in ret) if (v[key] === ret[e]) ret[e] = v; 
+    });
+    return ret;
+}
+
+Object.defineProperties(Array.prototype, {
+    firstObject: {
+        get: function () {
+            return this[0];
+        },
+    },
+    lastObject: {
+        get: function () {
+            return this[this.length - 1];
+        }
+    }
+});
+
+
+assert.deepStrictEqual(users.mapBy('id'), [1, 3, 2]);
+assert.deepStrictEqual(users.mapBy('name'), ['Hong', 'Lee', 'Kim']);
+assert.deepStrictEqual(users.filterBy('id', 2), [kim]);
+assert.deepStrictEqual(users.rejectBy('id', 2), [hong2, lee]);
+assert.deepStrictEqual(users.findBy('name', 'Kim'), kim);
+assert.deepStrictEqual(users.sortBy('name'), [hong2, kim, lee]);
+assert.deepStrictEqual(users.sortBy('name:desc'), [lee, kim, hong2]);
+assert.deepStrictEqual(users.firstObject, hong2);
+assert.deepStrictEqual(users.lastObject, kim);
+
+/////////////////////////////////////////////////////////////////////
+
+//class와 Array를 이용하여 Stack과 Queue를 구현하시오.
+
+//ex1) Stack
+class Stack {
+    constructor () {
+        this.ret = [];
+    }
+    push (num) {
+        this.ret.push(num);
+        return this.ret;
+    }
+    pop () {
+        return this.ret.pop();
+    }
+}
+
+const stack = new Stack(); // or new Stack([1,2]); // (1,2)
+stack.push(3); // 추가하기
+console.log(stack.pop()); // 마지막에 추가된 하나 꺼내기
+
+//ex2) Queue
+class Queue {
+    constructor () {
+        this.ret = [];
+    }
+    enqueue (num) {
+        this.ret.push(num);
+        return this.ret;
+    }
+    dequeue () {
+        return this.ret.shift();
+    }
+}
+const queue = new Queue();
+queue.enqueue(3); // 추가하기
+console.log(queue.dequeue()); // 추가한지 가장 오래된 - 먼저 들어간 - 하나 꺼내기
+
+/////////////////////////////////////////////////////////////
+
+//이전 장표에서 작성한 Stack과 Queue에 공통 기능을 확장하시오.(Collection)
+// 공통: clear(), toArray(), print(), remove(), isEmtpy, peek, poll, length(size)
+// peek: 가장 (Stack:나중, Queue:먼저) 들어간 요소 반환 (요소 삭제 없음!)
+// poll: 가장 (Stack:나중, Queue:먼저) 들어간 요소 반환 & 삭제 ⇐⇒ Stack.pop, Queue.dequeue
+// remove: 가장 (Stack:나중, Queue:먼저) 들어간 요소 삭제(skip)
+
+class Collection {
+    constructor () {
+        this.ret = [];
+        this.isStack = (this.pop) ? true : false;
+    }
+    get isEmpty () {
+        return (this.ret.length > 0) ? false : true;
+    }
+    get peek () {
+        return (this.isStack) ? this.ret.slice(this.length - 1) : this.ret.slice(0, 1);
+    }
+    get poll () {
+        return (this.isStack) ? this.pop() : this.dequeue();
+    }
+    get length () {
+        return this.ret.length;
+    }
+    clear () {
+        this.ret = [];
+    }
+    toArray () {
+        return this.ret;
+    }
+    print () {
+        console.log(this.ret);
+    }
+    remove () {
+        (this.isStack) ? this.pop() : this.dequeue();
+    }
+}
+
+class Stack2 extends Collection {
+    push (num) {
+        this.ret.push(num);
+        return this.ret;
+    }
+    pop () {
+        return this.ret.pop();
+    }
+}
+
+class Queue2 extends Collection{
+    enqueue (num) {
+        this.ret.push(num);
+        return this.ret;
+    }
+    dequeue () {
+        return this.ret.shift();
+    }
+}
+
+const stack2 = new Stack2;
+const queue2 = new Queue2;
+stack2.push(1); // 추가하기
+stack2.push(2); // 추가하기
+stack2.push(3); // 추가하기
+stack2.push(4); // 추가하기
+queue2.enqueue(5); // 추가하기
+queue2.enqueue(6); // 추가하기
+queue2.enqueue(7); // 추가하기
+queue2.enqueue(8); // 추가하기
+console.log(stack2);
+console.log(queue2);
+console.log(stack2.peek, queue2.peek); // 마지막(다음에 나올) 원소
+console.log(stack2.poll, queue2.poll); // 마지막(다음에 나올) 원소
+console.log(stack2);
+console.log(queue2);
+queue2.print(); // 출력해보기
+
+const arr2 = queue2.toArray().map(a => console.log(a));
+
+if (!stack2.isEmpty) stack2.clear();
+console.log(stack2);
+if (queue2.length) queue2.clear();
+console.log(queue2);
